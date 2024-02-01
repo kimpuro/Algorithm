d1e60371c3685e7900213f64b1e8da547d140ebb
KimPuro <98334072+KimPuro@users.noreply.github.com>
2024-02-02 03:08:31 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,19 @@
+int main(void){
+    int a;
+    scanf("%d",&a);
+    if(90<=a&&a<=100){
+        printf("A");
+    }
+    else if(80<=a&&a<=89){
+        printf("B");
+    }
+    else if(70<=a&&a<=79){
+        printf("C");
+    }
+    else if(60<=a&&a<=69){
+        printf("D");
+    }
+    else{
+        printf("F");
+    }
+}
