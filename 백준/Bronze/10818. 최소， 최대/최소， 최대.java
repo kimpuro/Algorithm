8285c7cbc9a844d4f20ed4e6aef80657cf94aaa6
DongEun <98334072+KimPuro@users.noreply.github.com>
2024-02-27 17:52:45 +0900

    [Bronze III] Title: 최소, 최대, Time: 2572 ms, Memory: 320996 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0]+" "+arr[n-1]);
+    }
+}
