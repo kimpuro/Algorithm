a2549e0869b25e8fb417378bf6de26e26e1b6c79
DongEun <98334072+KimPuro@users.noreply.github.com>
2024-03-03 16:15:10 +0900

    [Bronze V] Title: N 찍기, Time: 1000 ms, Memory: 33160 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a= sc.nextInt();
+        for(int i=1; i<=a;i++){
+            System.out.println(i);
+        }
+    }
+}
