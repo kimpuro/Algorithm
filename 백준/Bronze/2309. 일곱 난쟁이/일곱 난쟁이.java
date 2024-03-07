7f403b257f4fe6d74b2975c1acc101c99fd4ba2a
DongEun <98334072+KimPuro@users.noreply.github.com>
2024-03-08 02:08:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 132 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,16 +1,21 @@
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] numbers = new int[9];
         for (int i = 0; i < 9; i++) {
-            numbers[i] = scanner.nextInt();
+            numbers[i] = Integer.parseInt(br.readLine());
         }
-        findCombination(numbers);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        findCombination(numbers, bw);
+        bw.flush();
+
     }
 
-    public static void findCombination(int[] numbers) {
+    public static void findCombination(int[] numbers, BufferedWriter bw) throws IOException {
         int n = numbers.length;
         for (int i = 0; i < (1<<n); i++) {
             List<Integer> combination = new ArrayList<>();
@@ -20,7 +25,13 @@ public class Main {
                 }
             }
             if (combination.size() == 7 && combination.stream().mapToInt(Integer::intValue).sum() == 100) {
-                combination.stream().sorted().forEach(System.out::println);
+                combination.stream().sorted().forEach(e -> {
+                    try {
+                        bw.write(e + "\n");
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                });
                 break;
             }
         }
