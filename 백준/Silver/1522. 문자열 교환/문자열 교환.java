77289956bd749df9a157ab0ff5893c900ab00d2f
DongEun <98334072+KimPuro@users.noreply.github.com>
2024-03-07 21:50:12 +0900

    [Silver I] Title: 문자열 교환, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+
+        int aCount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a') {
+                aCount++;
+            }
+        }
+
+        int minB = Integer.MAX_VALUE;
+        int currentB = 0;
+
+        for (int i = 0; i < aCount; i++) {
+            if (str.charAt(i) == 'b') {
+                currentB++;
+            }
+        }
+
+        minB = Math.min(minB, currentB);
+
+        for (int i = 0; i < str.length(); i++) {
+            int startIdx = i;
+            int endIdx = (i + aCount) % str.length();
+
+            if (str.charAt(startIdx) == 'b') {
+                currentB--;
+            }
+
+            if (str.charAt(endIdx) == 'b') {
+                currentB++;
+            }
+
+            minB = Math.min(minB, currentB);
+        }
+
+        bw.write(String.valueOf(minB));
+        bw.flush();
+    }
+}
