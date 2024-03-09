7227e328a365ae2853ed20f33c1316ae669dbd3a
Dongeun <98334072+KimPuro@users.noreply.github.com>
2024-03-09 18:07:26 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args) throws IOException {
@@ -7,9 +8,9 @@ public class Main{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        String[] input = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for(int i=0; i<N; i++){
-            arr[i] = Integer.parseInt(input[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
         bw.write(String.valueOf(sumOfArr(arr)));
@@ -18,11 +19,11 @@ public class Main{
 
     public static int sumOfArr(int[] arr) {
         int sum = 0;
-        int acSum = 0;
+        int accumulatedSum = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            acSum += arr[i];
-            sum += acSum;
+            accumulatedSum += arr[i];
+            sum += accumulatedSum;
         }
 
         return sum;
