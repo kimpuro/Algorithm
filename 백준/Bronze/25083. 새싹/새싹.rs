02fccbf90f689635c037f13bc94262f193e662ce
Dongeun <98334072+KimPuro@users.noreply.github.com>
2024-03-12 22:04:52 +0900

    [Bronze V] Title: 새싹, Time: 4 ms, Memory: 13192 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("         ,r'\"7");
+    println!("r`-_   ,'  ,/");
+    println!(" \\. \". L_r'");
+    println!("   `~\\/");
+    println!("      |");
+    println!("      |");
+}
