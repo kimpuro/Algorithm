356d6c30b381367d9e863586570b2697d9e6dbe1
Dongeun <puro@protonmail.ch>
2024-04-19 22:59:46 +0900

    [Bronze V] Title: Pyramids, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+
+   input.forEach((el) => {
+       if(el==="0") {
+       }else {
+           let a=parseInt(el)
+           console.log(sum(a));
+       }
+    }
+   )
+
+function sum(n) {
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
