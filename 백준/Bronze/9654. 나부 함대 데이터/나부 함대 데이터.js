22bdc4e47fa4a8032213c62ec4c5b647d246e216
KimPuro <puro@protonmail.ch>
2024-04-26 23:54:16 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 112 ms, Memory: 9276 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.js
@@ -0,0 +1,6 @@
+console.log("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+console.log("N2 Bomber      Heavy Fighter  Limited    21        ");
+console.log("J-Type 327     Light Combat   Unlimited  1         ");
+console.log("NX Cruiser     Medium Fighter Limited    18        ");
+console.log("N1 Starfighter Medium Fighter Unlimited  25        ");
+console.log("Royal Cruiser  Light Combat   Limited    4         ");
