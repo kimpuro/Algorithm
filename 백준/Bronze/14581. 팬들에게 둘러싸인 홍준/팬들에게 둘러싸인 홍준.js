7eafc3668e227def701f12344d97cafcd61ccf0a
KimPuro <puro@protonmail.ch>
2024-04-29 23:54:19 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+console.log(":fan::fan::fan:");
+console.log(":fan::"+input+"::fan:");
+console.log(":fan::fan::fan:");
